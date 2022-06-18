c437bc728112f4d7dc34b60bd79e9925ef9338f3
AndreyKorovin <martovsky910@gmail.com>
2022-06-18 20:30:16 +0300

    create cintains function

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿string[] array1 = new string[5] { "cow", "cat", "hello", "world", "sun" };
 string[] array2 = new string[array1.Length];
 
+ContainsArray(array1, array2);
+
+void ContainsArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
